16ec2b6414797000b5d05bb9fb40e08a3a3b2b15
qas <simmaksimus@gmail.com>
2023-09-06 16:22:54 +0300

    optimized 14th

--- day_1/14.py
+++ day_1/14.py
@@ -1,10 +1,7 @@
 L = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e"]
-x = 0
-while True:
-    first = "123" + L[x] + '5'
-    second = "1" + L[x] + '233'
+for x in L:
+    first = "123" + x + '5'
+    second = "1" + x + '233'
     if (int(first, 15) + int(second, 15)) % 14 == 0:
         print((int(first, 15) + int(second, 15)) // 14)
         break
-    else:
-        x += 1
